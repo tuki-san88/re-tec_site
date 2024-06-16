72ebbd9ec50df9939dd1c941be7b577d1ddd8c19
sassy <luxeadd@gmail.com>
2024-06-16 12:57:40 +0900

    関連情報作成

--- page-company.php
+++ page-company.php
@@ -3,12 +3,46 @@
  * Template Name: 会社案内
  */
 ?>
+<!-- 各ページへのリンクfunction呼び出し -->
+<?php $urls = get_my_urls(); ?>
 <?php get_header(); ?>
 <main>
   <div class="sub">
-    ああああ
+
+
+    <!-- 関連情報 -->
+    <div class="subInfo">
+      <div class="inner">
+        <?php
+        $args = [
+          [
+            'link' => $urls['careers'],
+            'img' => 'sub/img_related_recruit.jpg',
+            'title' => '採用について',
+            'titleColor' => 'white',
+            'position' => 'positionTop',
+          ],
+          [
+            'link' => $urls['offices'],
+            'img' => 'sub/img_related_office.jpg',
+            'title' => '事業所一覧',
+            'titleColor' => 'white',
+            'position' => 'positionTop',
+          ],
+          [
+            'link' => $urls['contact'],
+            'img' => 'sub/img_related_contact.jpg',
+            'title' => 'お問い合わせ',
+            'titleColor' => 'gray',
+            'position' => 'positionBottom',
+          ],
+        ];
+        get_template_part('tmp/relatedInfo', null, $args);
+        ?>
+      </div>
+    </div>
+
+
   </div>
 </main>
-
-
 <?php get_footer(); ?>
