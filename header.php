d1676b06e3e59d60f4fe9ee62150593f01da331e
tuki-san88 <yuubou87854@yahoo.co.jp>
2024-06-25 15:07:22 +0900

    ファビコンの設定

--- header.php
+++ header.php
@@ -13,6 +13,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no" />
     <title><?php the_title(); ?></title>
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon">
+
     <?php wp_head(); ?>
 </head>
 
